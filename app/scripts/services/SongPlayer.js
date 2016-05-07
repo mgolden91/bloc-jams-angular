3a343f4af2bf8d2837fb2923daf628cd45237a9b
Matthew Golden <mgolden91@gmail.com>
2016-05-07 11:12:04 -0500

    assignment7

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,6 +1,10 @@
 (function() {
      function SongPlayer() {
           var SongPlayer = {};
+          /**
+          * @desc current song 
+          * @type {Object}
+          */
           var currentSong = null;
           /**
           * @desc Buzz object audio file
@@ -23,19 +27,36 @@
               });
               currentSong = song;
           };
+          /**
+          * @function playSong
+          * @desc Plays currentBuzzObject and sets song.playing to true
+          * @param {Object} song
+          */
+          var playSong = function(song){
+              currentBuzzObject.play();
+              song.playing = true;
+          };
          
+         /**
+          * @function SongPlayer.play
+          * @desc Sets and plays current song
+          * @param {Object} song
+          */
           SongPlayer.play = function(song){
               if (currentSong !== song){
                   setSong(song);
-                  currentBuzzObject.play();
-                  song.playing = true;
-              } else if (currentSong === song){
+                  playSong(song);
+              } else if (currentSong === song){     
                   if (currentBuzzObject.isPaused()){
-                      currentBuzzObject.play();
+                      playSong(song);
                   }
               }
           };
-         
+         /**
+          * @function SongPlayer.pause
+          * @desc Pauses current song
+          * @param {Object} song
+          */
           SongPlayer.pause = function(song){
               currentBuzzObject.pause();
               song.playing = false;
